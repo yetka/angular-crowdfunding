76f07da809fe783d14cda89c5463367a03d1dc7c
Guest User <Guest@epicodus-7.local>
2018-01-25 10:49:35 -0800

    add option to edit projects

--- src/app/edit-project/edit-project.component.ts
+++ src/app/edit-project/edit-project.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Project } from '../project.model';
 import { ProjectService } from '../project.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-edit-project',
@@ -9,11 +11,19 @@ import { ProjectService } from '../project.service';
   providers: [ProjectService]
 })
 export class EditProjectComponent implements OnInit {
+  // projectToDisplay needs to be defined to access object in database
+  projectToDisplay = Project;
+  projectId: string;
 
-
-  constructor(private projectService: ProjectService) { }
+  constructor(private route: ActivatedRoute, private location: Location, private projectService: ProjectService) { }
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.projectId = urlParameters['id'];
+    });
+    this.projectService.getProjectById(this.projectId).subscribe(dataLastEmittedFromObserver => {
+      this.projectToDisplay = dataLastEmittedFromObserver;
+    });
   }
 
   beginUpdatingProject(projectToUpdate) {
